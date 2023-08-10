1fa0c7e379b7a10e93f2931ec29811c3c6a16ecb
Lucas-E <lucasgitacc@gmail.com>
2023-08-10 10:09:33 -0300

    added footer

---
+++ aulas/projeto-livraria/Library-Project/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+        <div className="p-3 w-full bg-projectBlue text-lg text-white">
+            FEITO COM REACT E ❤️ por LUCAS-E
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Footer
--- aulas/projeto-livraria/Library-Project/src/pages/Index/Index.tsx
+++ aulas/projeto-livraria/Library-Project/src/pages/Index/Index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import EbookSection from '../../components/EbookSection/EbookSection'
+import Footer from '../../components/Footer/Footer'
 const Index = () => {
   return (
     <React.Fragment>
         <Header/>
         <EbookSection/>
+        <Footer/>
     </React.Fragment>
   )
 }

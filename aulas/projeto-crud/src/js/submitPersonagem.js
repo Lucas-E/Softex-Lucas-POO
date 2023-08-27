f57bd7db333a380f1873ed5f08c2120e5ba3cf85
Lucas-E <lucasgitacc@gmail.com>
2023-08-27 18:50:42 -0300

    adicionei funcionalidade para ver dados de ficha

--- aulas/projeto-crud/src/js/submitPersonagem.js
+++ aulas/projeto-crud/src/js/submitPersonagem.js
@@ -12,9 +12,14 @@ function renderizaPlayers() {
           <h5 class="card-title">${player?.nome ? player.nome : "No name"}</h5>
           <h6 class="card-subtitle mb-2 mt-2 text-muted">id ${player.id}</h6>
           <p class="card-text">${player.descricao}</p>
-          <button class="btn btn-danger" id="character${player.id}">apagar</button>
-          <button class="btn btn-danger">editar</button>
-          <button class="btn btn-danger">ver</button>
+          <button class="btn btn-danger" id="character${
+				player.id
+			}">apagar</button>
+          <button class="btn btn-danger" id="editar${player.id}">editar</button>
+          <button class="btn btn-danger" id="ler${
+				player.id
+			}" data-bs-toggle="modal"
+          data-bs-target="#lerFichaModal">ver</button>
         </div>
       </div>
         `;
@@ -22,17 +27,38 @@ function renderizaPlayers() {
 		wrapper.innerHTML = templating;
 		fichasElement.appendChild(wrapper);
 
+		let deletarButton = document.getElementById(`character${player.id}`);
+		deletarButton.addEventListener("click", (e) => {
+			deletarPersonagem(player.id);
+		});
 
-        let deletarButton = document.getElementById(`character${player.id}`);
-        deletarButton.addEventListener('click', (e) => {
-            deletarPersonagem(player.id)
-        })
+		let readButton = document.getElementById(`ler${player.id}`);
+		readButton.addEventListener("click", (e) => {
+			lerDadosPersonagem(player);
+		});
 	}
 }
 
 function deletarPersonagem(id) {
 	fichas.deletePlayerById(id);
 	renderizaPlayers();
+}
+
+function lerDadosPersonagem(player) {
+	const dadosTemplate = `
+        <div class="d-flex flex-column">
+            <span><h2>Nome: </h2> ${player.nome} </span>
+            <span><h2>Profissão: </h2> ${player.profissao} </span>
+            <span><h2>Força: </h2> ${player.forca} </span>
+            <span><h2>Vida: </h2> ${player.vida} </span>
+            <span><h2>Mana: </h2> ${player.mana} </span>
+            <span><h2>Inteligência: </h2> ${player.inteligencia} </span>
+            <span><h2>Descrição: </h2> ${player.descricao} </span>
+        </div>
+    `;
+
+    const modalVisualizacao = document.getElementById('verPersonagemBody')
+    modalVisualizacao.innerHTML = dadosTemplate
 }
 
 function submitPersonagem(event) {

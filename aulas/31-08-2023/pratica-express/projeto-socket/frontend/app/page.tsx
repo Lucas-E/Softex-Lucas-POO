b9b1f6cfda010dfc79ae5c105a74c811d511433d
Lucas-E <lucasgitacc@gmail.com>
2023-08-31 23:51:16 -0300

    identei

--- aulas/31-08-2023/pratica-express/projeto-socket/frontend/app/page.tsx
+++ aulas/31-08-2023/pratica-express/projeto-socket/frontend/app/page.tsx
@@ -1,52 +1,62 @@
-'use client'
-import { useState, useEffect } from "react"
+"use client";
+import { useState, useEffect } from "react";
 import getSocket from "@/utils/services/socket";
 import { io } from "socket.io-client";
 
-export interface message{
-  isMine: boolean,
-  content: string
+export interface message {
+	isMine: boolean;
+	content: string;
 }
 
 export default function Home() {
-  const [isConnected, setIsConnected] = useState<boolean>(false);
-  const [messages, setMessages] = useState<Array<message>>([]);
-  const [typedMessage, setTypedMessage] = useState<string>()
-  const [socket, setSocket] = useState<ReturnType<typeof io>>()
+	const [isConnected, setIsConnected] = useState<boolean>(false);
+	const [messages, setMessages] = useState<Array<message>>([]);
+	const [typedMessage, setTypedMessage] = useState<string>();
+	const [socket, setSocket] = useState<ReturnType<typeof io>>();
 
-  useEffect(() => {
-    const socketRef = getSocket(setIsConnected, setMessages)
-    setSocket(socketRef)
-    if(socketRef.connected){
-      setIsConnected(true);
-    }
-  }, [])
+	useEffect(() => {
+		const socketRef = getSocket(setIsConnected, setMessages);
+		setSocket(socketRef);
+		if (socketRef.connected) {
+			setIsConnected(true);
+		}
+	}, []);
 
-  const handleSendMessage = (e:React.MouseEvent<HTMLElement>) => {
-    e.preventDefault()
-    if(socket){
-      socket.emit('sendMessage', typedMessage)
-    }
-    setTypedMessage('')
-  }
+	const handleSendMessage = (e: React.MouseEvent<HTMLElement>) => {
+		e.preventDefault();
+		if (socket) {
+			socket.emit("sendMessage", typedMessage);
+		}
+		setTypedMessage("");
+	};
 
-  const handleMessage = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setTypedMessage(e.target.value)
-  }
+	const handleMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTypedMessage(e.target.value);
+	};
 
-  return (
-    <>
-    <div className="text-3xl font-bold underline">
-      {messages.map((message) => (
-        <p>
-        {message.content}
-      </p>
-      ))}
-    </div>
-    <div className="fixed bottom-0 rounded w-screen flex justify-around">
-      <input type="text" className="border p-5 w-full" placeholder="digite sua mensagem" onChange={handleMessage} value={typedMessage}/>
-      <button onClick={handleSendMessage} className="border rounded p-5 bg-blue-600 active:bg-blue-800 text-white " type="button" >teste</button>
-    </div>
-    </>
-  )
+	return (
+		<>
+			<div className="text-3xl font-bold underline">
+				{messages.map((message) => (
+					<p>{message.content}</p>
+				))}
+			</div>
+			<div className="fixed bottom-0 rounded w-screen flex justify-around">
+				<input
+					type="text"
+					className="border p-5 w-full"
+					placeholder="digite sua mensagem"
+					onChange={handleMessage}
+					value={typedMessage}
+				/>
+				<button
+					onClick={handleSendMessage}
+					className="border rounded p-5 bg-blue-600 active:bg-blue-800 text-white "
+					type="button"
+				>
+					teste
+				</button>
+			</div>
+		</>
+	);
 }
--- aulas/31-08-2023/pratica-express/projeto-socket/frontend/utils/services/socket.ts
+++ aulas/31-08-2023/pratica-express/projeto-socket/frontend/utils/services/socket.ts
@@ -1,10 +1,13 @@
 import { io } from "socket.io-client";
 import { message } from "@/app/page";
 
-export default function getSocket(setIsConnected:any, setMessages:any):ReturnType<typeof io>{
-    const socket = io('http://localhost:8080');
-    socket.on('recievedMessage', (data) => {
-        setMessages((prev:Array<message>) => ([...prev, data]))
-    })
-    return socket
+export default function getSocket(
+	setIsConnected: any,
+	setMessages: any
+): ReturnType<typeof io> {
+	const socket = io("http://localhost:8080");
+	socket.on("recievedMessage", (data) => {
+		setMessages((prev: Array<message>) => [...prev, data]);
+	});
+	return socket;
 }
